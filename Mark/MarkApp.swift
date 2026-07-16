65f1e24bb30c7cf2fc1d9f2b1bd52d5b46ba1463
weichen.wang <weichen.wang@lotusflare.com>
2026-07-16 10:58:12 +0800

    Initial Commit

---
+++ Mark/MarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkApp.swift
+//  Mark
+//
+//  Created by Walt Wang on 2026-07-16.
+//
+
+import SwiftUI
+
+@main
+struct MarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
